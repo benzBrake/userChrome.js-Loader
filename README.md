b55d1892ea2834af7654a62f7843892faf1d76f7
Ryan Lieu <github-benzBrake@woai.ru>
2024-04-29 09:11:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# userChrome.js 环境
+
+
+
+基于 alice0775 环境打包，加入了来自 xiaoxiaoflood 环境的 Bootstrap Loader，支持 onlyonce 属性，使用这个 userChrome.js 可以安装适配过的传统扩展，部分 xiaoxiaoflood 的脚本也可以无需移植就能使用。
+
+实际上可以向下兼容，具体版本没有测试。
+
+## 使用说明
+
+解压后最多有两个目录，`program`目录里的东西要解压到 Firefox.exe 所在目录，`profile`目录里的文件要解压到配置文件夹。
+
+#### 如何查找 Firefox.exe 所在目录 和 配置文件夹？看图
+
+![排障信息](support.jpg)
+
+#### 安装 userChrome.js 环境后如何安装脚本？
+
+下载 `.uc.js` 后缀的文件保存到**配置文件夹**下的**chrome**文件夹下
+
+![安装脚本](install-scripts.png)
+
+## 兼容的传统扩展
+
+https://github.com/xiaoxiaoflood/firefox-scripts/tree/master/extensions
+
---
+++ program/config.js
@@ -0,0 +1,222 @@
+// skip 1st line
+/*
+ 2023/07/11 Removed Services.jsm, per Bug 1780695
+ 2022/06/07 remove osfile.jsm
+ 2021/08/05 fix for 92+ port Bug 1723723 - Switch JS consumers from getURLSpecFromFile to either getURLSpecFromActualFile or getURLSpecFromDir
+ 2019/12/11 01:30 fix 72 use "load" in config.js, working with Sub-Script/Overlay Loader v3.0.60mod
+ 2019-10-22 23:00
+*/
+lockPref("toolkit.telemetry.enabled", false);
+
+try {
+
+  let {
+    classes: Cc,
+    interfaces: Ci,
+    manager: Cm,
+    utils: Cu
+  } = Components;
+
+  //Cu.import('resource://gre/modules/osfile.jsm');
+
+  let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
+  cmanifest.append('utils');
+  cmanifest.append('chrome.manifest');
+
+  if (cmanifest.exists()) {
+    Cm.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
+    // https://forum.mozilla-russia.org/viewtopic.php?pid=808453#p808453
+    try {
+      (jsval => {
+        var dbg, gref, genv = func => {
+          var sandbox = new Cu.Sandbox(g, { freshCompartment: true });
+          Cc["@mozilla.org/jsdebugger;1"].createInstance(Ci.IJSDebugger).addClass(sandbox);
+          (dbg = new sandbox.Debugger()).addDebuggee(g);
+          gref = dbg.makeGlobalObjectReference(g);
+          return (genv = func => func && gref.makeDebuggeeValue(func).environment)(func);
+        }
+        var g = Cu.getGlobalForObject(jsval), o = g.Object, { freeze } = o, disleg;
+
+        var lexp = () => lockPref("extensions.experiments.enabled", true);
+        var MRS = "MOZ_REQUIRE_SIGNING", AC = "AppConstants", uac = `resource://gre/modules/${AC}.`;
+
+        if (o.isFrozen(o)) { // Fx 102.0b7+
+          lexp(); disleg = true; genv();
+
+          dbg.onEnterFrame = frame => {
+            var { script } = frame;
+            try { if (!script.url.startsWith(uac)) return; } catch { return; }
+            dbg.onEnterFrame = undefined;
+
+            if (script.isModule) { // ESM, Fx 108+
+              var env = frame.environment;
+              frame.onPop = () => env.setVariable(AC, gref.makeDebuggeeValue(freeze(
+                o.assign(new o(), env.getVariable(AC).unsafeDereference(), { [MRS]: false })
+              )));
+            }
+            else { // JSM
+              var nsvo = frame.this.unsafeDereference();
+              nsvo.Object = {
+                freeze(ac) {
+                  ac[MRS] = false;
+                  delete nsvo.Object;
+                  return freeze(ac);
+                }
+              };
+            }
+          }
+        }
+        else o.freeze = obj => {
+          if (!Components.stack.caller.filename.startsWith(uac)) return freeze(obj);
+          obj[MRS] = false;
+
+          if ((disleg = "MOZ_ALLOW_ADDON_SIDELOAD" in obj)) lexp();
+          else
+            obj.MOZ_ALLOW_LEGACY_EXTENSIONS = true,
+              lockPref("extensions.legacy.enabled", true);
+
+          return (o.freeze = freeze)(obj);
+        }
+        lockPref("xpinstall.signatures.required", false);
+        lockPref("extensions.langpacks.signatures.required", false);
+
+        var useDbg = true, xpii = "resource://gre/modules/addons/XPIInstall.";
+        if (Ci.nsINativeFileWatcherService) { // Fx < 100
+          jsval = Cu.import(xpii + "jsm", {});
+          var shouldVerify = jsval.shouldVerifySignedState;
+          if (shouldVerify.length == 1)
+            useDbg = false,
+              jsval.shouldVerifySignedState = addon => !addon.id && shouldVerify(addon);
+        }
+        if (useDbg) { // Fx 99+
+          try { var exp = ChromeUtils.importESModule(xpii + "sys.mjs"); }
+          catch { exp = g.ChromeUtils.import(xpii + "jsm"); }
+          jsval = o.assign({}, exp);
+
+          var env = genv(jsval.XPIInstall.installTemporaryAddon);
+          var ref = name => { try { return env.find(name).getVariable(name).unsafeDereference(); } catch { } }
+          jsval.XPIDatabase = (ref("XPIExports") || ref("lazy") || {}).XPIDatabase || ref("XPIDatabase");
+
+          var proto = ref("Package").prototype;
+          var verify = proto.verifySignedState;
+          proto.verifySignedState = function (id) {
+            return id ? { cert: null, signedState: undefined } : verify.apply(this, arguments);
+          }
+          dbg.removeAllDebuggees();
+        }
+        if (disleg) jsval.XPIDatabase.isDisabledLegacy = () => false;
+      })(
+        "permitCPOWsInScope" in Cu ? Cu.import("resource://gre/modules/WebRequestCommon.jsm", {}) : Cu
+      );
+    }
+    catch (ex) { Cu.reportError(ex); }
+
+    Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
+  }
+} catch (ex) { Cu.reportError(ex); }
+
+try {
+  Cu.import('resource://gre/modules/Services.jsm');
+} catch (ex) { }
+
+try {
+  const { AppConstants } = parseInt(Services.appinfo.version) < 108 ? ChromeUtils.import('resource://gre/modules/AppConstants.jsm') : ChromeUtils.importESModule("resource://gre/modules/AppConstants.sys.mjs");
+
+  let UC = {
+    webExts: new Map(),
+    sidebar: new Map()
+  }
+
+  let _uc = {
+    BROWSERCHROME: AppConstants.MOZ_APP_NAME == 'thunderbird' ? 'chrome://messenger/content/messenger.xhtml' : 'chrome://browser/content/browser.xhtml',
+    BROWSERTYPE: AppConstants.MOZ_APP_NAME == 'thunderbird' ? 'mail:3pane' : 'navigator:browser',
+    BROWSERNAME: AppConstants.MOZ_APP_NAME.charAt(0).toUpperCase() + AppConstants.MOZ_APP_NAME.slice(1),
+    sss: Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService),
+    chromedir: Services.dirsvc.get('UChrm', Ci.nsIFile),
+    scriptsDir: '',
+
+    get isFaked() {
+      return true;
+    },
+
+    windows: function (fun, onlyBrowsers = true) {
+      let windows = Services.wm.getEnumerator(onlyBrowsers ? this.BROWSERTYPE : null);
+      while (windows.hasMoreElements()) {
+        let win = windows.getNext();
+        if (!win._uc)
+          continue;
+        if (!onlyBrowsers) {
+          let frames = win.docShell.getAllDocShellsInSubtree(Ci.nsIDocShellTreeItem.typeAll, Ci.nsIDocShell.ENUMERATE_FORWARDS);
+          let res = frames.some(frame => {
+            let fWin = frame.domWindow;
+            let { document, location } = fWin;
+            if (fun(document, fWin, location))
+              return true;
+          });
+          if (res)
+            break;
+        } else {
+          let { document, location } = win;
+          if (fun(document, win, location))
+            break;
+        }
+      }
+    },
+
+    createElement: function (doc, tag, atts, XUL = true) {
+      let el = XUL ? doc.createXULElement(tag) : doc.createElement(tag);
+      for (let att in atts) {
+        el.setAttribute(att, atts[att]);
+      }
+      return el
+    }
+  }
+
+  function UserChrome_js() {
+    Services.obs.addObserver(this, 'domwindowopened', false);
+  };
+
+  UserChrome_js.prototype = {
+    observe: function (aSubject, aTopic, aData) {
+      aSubject.addEventListener('load', this, true);
+    },
+
+    handleEvent: function (aEvent) {
+      let document = aEvent.originalTarget;
+      let window = document.defaultView;
+      let location = window.location;
+      if (location && location.protocol == 'chrome:') {
+        const ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+        const fph = ios.getProtocolHandler("file").QueryInterface(Ci.nsIFileProtocolHandler);
+        const ds = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties);
+
+        const { xPref } = parseInt(Services.appinfo.version) >= 115 ? ChromeUtils.importESModule("chrome://userchromejs/content/xPref.sys.mjs") : Cu.import("chrome://userchromejs/content/xPref.jsm");
+        window.xPref = xPref;
+
+        window.UC = UC;
+        window._uc = _uc;
+
+        if (window._gBrowser) // bug 1443849
+          window.gBrowser = window._gBrowser;
+
+        let file = ds.get("UChrm", Ci.nsIFile);
+        file.append('userChrome.js');
+        let fileURL = fph
+          .getURLSpecFromActualFile(file) + "?" + file.lastModifiedTime;
+        Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Ci.mozIJSSubScriptLoader)
+          .loadSubScript(fileURL, document.defaultView, 'UTF-8');
+      }
+    },
+  };
+
+
+
+  if (!Cc['@mozilla.org/xre/app-info;1'].getService(Ci.nsIXULRuntime).inSafeMode)
+    new UserChrome_js();
+
+
+} catch (ex) { Cu.reportError(ex); };
+
+try {
+  pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+} catch (e) { }
